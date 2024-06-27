debf16738a8b3f6b9247647cceaeb21269d46610
Andy Friedman <afriedman412@gmail.com>
2024-06-27 18:31:07 -0400

    code cleaned up a bit, post ID now in sentiment analysis results, config has some more options

--- sentiment_analysis_src/nlp_config.py
+++ sentiment_analysis_src/nlp_config.py
@@ -1,3 +1,9 @@
+OUTPUT_FILE = 'output_clean.csv'
+INPUT_FILE = 'sentiment_data.csv'
+TEXT_COL = "selftext"
+ID_COL = 'id'
+
+MODEL_PATH = "cardiffnlp/twitter-roberta-base-sentiment"
+
 BATCH_SIZE = 256
-OUTPUT_FILE = 'output.csv'
 PLACEHOLDER = {'label': 'na', 'score': 0}
